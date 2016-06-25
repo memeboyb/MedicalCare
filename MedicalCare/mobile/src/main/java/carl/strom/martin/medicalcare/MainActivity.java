702a1391bb2a066e0ad41b88b74323dac7fdea8b
memeboyb <martinstr982@gmail.com>
2016-06-25 23:49:28 +0200

    update

--- MedicalCare/mobile/src/main/java/carl/strom/martin/medicalcare/MainActivity.java
+++ MedicalCare/mobile/src/main/java/carl/strom/martin/medicalcare/MainActivity.java
@@ -1,5 +1,9 @@
 package carl.strom.martin.medicalcare;
 
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
@@ -7,9 +11,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.app.AlarmManager;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private PendingIntent pendingIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +55,77 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        setAlarm();
+        saveInfo();
+        readInfo();
     }
+
+    public boolean saveArray(String[] array, String arrayName, Context mContext) {//http://www.sherif.mobi/2012/05/string-arrays-and-object-arrays-in.html
+        SharedPreferences prefs = mContext.getSharedPreferences("carl.strom.martin.medicalcare", 0);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt(arrayName + "_size", array.length);
+        for(int i=0;i<array.length;i++)
+            editor.putString(arrayName + "_" + i, array[i]);
+        return editor.commit();
+    }
+
+    public String[] loadArray(String arrayName, Context mContext) {//http://www.sherif.mobi/2012/05/string-arrays-and-object-arrays-in.html
+        SharedPreferences prefs = mContext.getSharedPreferences("carl.strom.martin.medicalcare", 0);
+        int size = prefs.getInt(arrayName + "_size", 0);
+        String array[] = new String[size];
+        for(int i=0;i<size;i++)
+            array[i] = prefs.getString(arrayName + "_" + i, null);
+        return array;
+    }
+
+
+    void saveInfo(){
+        String medicineNames[] = {"a", "b", "c"};
+        String timeH[] = {"10","15","20"};
+        String timeM[] = {"00", "20", "00"};
+        saveArray(medicineNames,"medicineNames", this);
+    }
+
+    void readInfo(){
+       // String loadedArray[] = loadArray("medicineNames", this);
+       // Toast.makeText(this, loadedArray[0], Toast.LENGTH_SHORT).show();
+    }
+
+    void setAlarm() {
+       String loadedArray[] = loadArray("medicineNames", this);
+       for(int i = 0; i < 3; i++) {
+            Intent alarmIntent = new Intent(MainActivity.this, AlarmReceiver.class);
+            alarmIntent.putExtra("keyName", i);
+            pendingIntent = PendingIntent.getBroadcast(MainActivity.this, i, alarmIntent, 0);
+
+
+            AlarmManager manager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+            int interval = 8000;
+
+            manager.setInexactRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), interval, pendingIntent);
+            Toast.makeText(this, "setAlarm()", Toast.LENGTH_SHORT).show();
+       }
+    }
+    /*void setAlarm(){
+        Intent alarmIntent = new Intent(MainActivity.this, AlarmReceiver.class);
+        pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0, alarmIntent, 0);
+
+        AlarmManager manager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        int interval = 1000 * 60 * 20;
+
+        /* Set the alarm to start at 10:30 AM *//*
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        calendar.set(Calendar.HOUR_OF_DAY, 21);
+        calendar.set(Calendar.MINUTE, 48);
+
+        /* Repeating on every 20 minutes interval *//*
+        manager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+                1000 * 60 * 20, pendingIntent);
+
+        Toast.makeText(this, "Alarm set", Toast.LENGTH_SHORT).show();
+        // manager.setExactAndAllowWhileIdle(int type, long triggerAtMillis, PendingIntent operation);
+    }*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- MedicalCare/mobile/src/main/java/carl/strom/martin/medicalcare/TabFragment1.java
+++ MedicalCare/mobile/src/main/java/carl/strom/martin/medicalcare/TabFragment1.java
@@ -33,58 +33,5 @@ public class TabFragment1 extends ListFragment implements OnItemClickListener {
 }
 
 
-/*import android.app.ListFragment;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-
-public class TabFragment1 extends ListFragment {
-
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        populateListView();
-        return inflater.inflate(R.layout.tab_fragment_1, container, false);
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        populateListView();
-        //registerClickCallback();
-    }
-
-    private void populateListView() {
-        // Create list of items
-        String[] myItems = {"Blue", "Green", "Purple", "Red"}; // Build Adapter
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                this.getActivity(), // Context for the activity.
-                R.layout.text_view_item, // Layout to use
-                myItems); // Items to be displayed
-
-        // Configure the list view.
-        ListView list = (ListView) view.findViewById(R.id.listView);
-        list.setAdapter(adapter);
-    }
-
-    /*private void registerClickCallback() {
-        ListView list = (ListView) findViewById(R.id.listView);
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-
-            public void onItemClick(AdapterView<?> paret, View viewClicked, int position, long id) {
-                TextView textView = (TextView) viewClicked;
-                String message = "You clicked # " + position + ", which is string: " + textView.getText().toString();
-                Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
-            }
-        });
-}
-*/
-
-
 
 
